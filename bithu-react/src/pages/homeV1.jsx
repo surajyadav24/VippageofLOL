6e3faad70ef65527e3f22e975fe15fbd97da0d3a
suraj kumar yadav <84555072+surajyadav24@users.noreply.github.com>
2022-07-02 17:24:07 +0530

    unusede file deleted

--- bithu-react/src/pages/homeV1.jsx
+++ bithu-react/src/pages/homeV1.jsx
@@ -1,16 +1,14 @@
-
+import React, { Suspense, lazy } from "react";
 import { useModal } from "../utils/ModalContext";
 import loadable from '@loadable/component'
 import GlobalStyles from "../assets/styles/GlobalStyles";
 import Header from "../components/section/header/v1/Header";
-import Layout from "../common/layout";
-
-
+import Layout from "../common/layout/Layout"
+const Banner = lazy(() => import("../components/section/banner/v1"));
 // import Footer from "../components/section/footer/v1";
 // import Footer from "../components/section/footer/v1";
 // import MintNowModal from "../common/modal/mintNowModal";
 // import WalletModal from "../common/modal/walletModal/WalletModal";
-import Banner from "../components/section/banner/v1";
 // import Smartcontract from "../components/section/smartcontract/Smartcontract";
 const Footer = loadable(() => import('../components/section/footer/v1'))
 const HomeV1 = () => {
@@ -21,7 +19,9 @@ const HomeV1 = () => {
       {/* {visibility && <MintNowModal />}
       {walletModalvisibility && <WalletModal />} */}
       <Header />
+      <Suspense fallback={<div>Loading...</div>}>
       <Banner />
+      </Suspense>
       <Footer />
     </Layout>
   );
