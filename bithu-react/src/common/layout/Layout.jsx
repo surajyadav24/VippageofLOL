82cfee96e96467ae34b5699a907a1c4e7decd9f6
suraj kumar yadav <84555072+surajyadav24@users.noreply.github.com>
2022-07-02 17:24:27 +0530

    unused

--- bithu-react/src/common/layout/Layout.jsx
+++ bithu-react/src/common/layout/Layout.jsx
@@ -25,16 +25,8 @@ const Layout = ({ children }) => {
           href="https://fonts.googleapis.com/css2?family=Bakbak+One&family=Inter:wght@300;400&display=swap"
           rel="stylesheet"
         />
-        <link
-          rel="stylesheet"
-          type="text/css"
-          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
-        />
-        <link
-          rel="stylesheet"
-          type="text/css"
-          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
-        />
+  
+   
       </Helmet>  
       {children} 
     </>
